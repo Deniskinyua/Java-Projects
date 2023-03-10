d785286b8bb083fe7b91f40771fd0837fefd351f
Denis Kinyua <kimenjudenis@gmail.com>
2023-03-10 08:49:40 +0300

    Rectifying Errors

--- src/main/java/dev/kinyua/movies/MovieController.java
+++ src/main/java/dev/kinyua/movies/MovieController.java
@@ -1,9 +1,8 @@
 package dev.kinyua.movies;
 
-<<<<<<< HEAD
+
 import org.bson.types.ObjectId;
-=======
->>>>>>> e18ff8c84ca1d4d17baa6fc52125d2aba26d3807
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,26 +12,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-<<<<<<< HEAD
+
 import java.util.Optional;
-=======
->>>>>>> e18ff8c84ca1d4d17baa6fc52125d2aba26d3807
+
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
     @Autowired
     private MovieService movieService;
+
     @GetMapping
-    public ResponseEntity<List<Movie>> getAllMovies(){
+    public ResponseEntity<List<Movie>> getAllMovies() {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
-<<<<<<< HEAD
+
     }
+
     @GetMapping("/{imdbId}")
-    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
         return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId),
                 HttpStatus.OK);
-=======
->>>>>>> e18ff8c84ca1d4d17baa6fc52125d2aba26d3807
+
     }
 }
--- src/main/java/dev/kinyua/movies/MovieService.java
+++ src/main/java/dev/kinyua/movies/MovieService.java
@@ -1,17 +1,16 @@
 package dev.kinyua.movies;
 
-<<<<<<< HEAD
+
 import org.bson.types.ObjectId;
-=======
->>>>>>> e18ff8c84ca1d4d17baa6fc52125d2aba26d3807
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-<<<<<<< HEAD
+
 import java.util.Optional;
-=======
->>>>>>> e18ff8c84ca1d4d17baa6fc52125d2aba26d3807
+
 
 @Service
 public class MovieService {
@@ -20,10 +19,9 @@ public class MovieService {
     public List<Movie> allMovies(){
         return movieRepository.findAll();
     }
-<<<<<<< HEAD
+
     public Optional<Movie> singleMovie(String imdbId){
         return movieRepository.findMovieByImdbId(imdbId);
     }
-=======
->>>>>>> e18ff8c84ca1d4d17baa6fc52125d2aba26d3807
+
 }
